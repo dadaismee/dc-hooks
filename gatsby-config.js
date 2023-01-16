96deb17164efcfed181c8530377aa358c9259766
Valerii Shevchenko <46709216+dadaismee@users.noreply.github.com>
2023-01-16 15:08:07 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `React Hooks`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
